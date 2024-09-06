ae6b7ac81e6848e6d2db09b6e048bc4a60bca862
Anthoonyyy <Anthony212007@hotmail.com>
2024-09-06 15:11:03 +0200

    composant todolist à corriger

---
+++ FirstProject/firstproject/src/ToDoList.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import {useState} from "react";
+
+
+function ToDoList(){
+    const items = [
+        { id: 1, txt: 'Apprendre react'},
+        { id: 2, txt: 'Créer un projet'},
+        { id: 3, txt: 'Devenir expert'}
+          ];
+    return(
+    <ul>
+        {items.map(todo => (
+            <li key={todo.id}>{todo.txt}</li>
+        ))}
+    </ul>
+    );
+}
+
+export default ToDoList;
