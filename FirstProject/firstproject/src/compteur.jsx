0e9807e2aa1c7251211509339d1cd2f14a0d1e6d
Anthoonyyy <Anthony212007@hotmail.com>
2024-09-06 15:10:25 +0200

    composant compteur (nombre de fois cliqué)

---
+++ FirstProject/firstproject/src/compteur.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState } from 'react';
+
+function Count(){
+   const [count, setCount] = useState(0);
+
+    function Click(){
+   setCount(count + 1);
+   }
+   return(
+       <button onClick={Click}>
+           Nombre de clics : {count}
+       </button>
+   );
+}
+
+export default Count;
