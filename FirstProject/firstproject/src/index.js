5043c8c64a86f8a23afdae73416e21b9fa911c4c
Anthoonyyy <Anthony212007@hotmail.com>
2024-09-09 15:08:16 +0200

    appel des differents composant + installation boot et tail

--- FirstProject/firstproject/src/index.js
+++ FirstProject/firstproject/src/index.js
@@ -2,21 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Exe1 from "./exe1";
-import Button from "./button";
 import reportWebVitals from './reportWebVitals';
+import Exe1 from "./Composants/exe1";
+import Button1 from "./Composants/button1";
 import ToDoList from "./ToDoList";
-import Count from "./compteur";
+import Count from "./Composants/compteur";
+import TypesExample from "./Composants/TypesExample";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Pages/Layout";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Error404 from "./Pages/Error404";
+import ImageHome from "./Composants/ImageHome";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Exe1 message = "Hello React 2" />
-      <Button text = "Hello"  size = "2em" background = "purple" />
-      <Count />
-    <ToDoList/>
+      {/* <Exe1 message = "Hello React 2" /> */}
+      {/* <Button1 text = "Hello"  size = "2em" background = "purple" /> */}
+      {/*  <Count /> */}
+      {/* <ToDoList/> */}
+      {/* <TypesExample /> */}
 
-
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Layout />}>
+                  <Route index element={<Home />}/>
+                  <Route path="About" element={<About />}/>
+                  <Route path="Contact" element={<Contact />}/>
+                  <Route path="*" element={<Error404 />}/>
+              </Route>
+          </Routes>
+      </BrowserRouter>
+<ImageHome />
   </React.StrictMode>
 );
 
